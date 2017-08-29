cc6f9774726b286773a51892465fdacf30f4e80f
yashpatel5400 <ypatel@princeton.edu>
2017-08-28 20:26:11 -0700

    Added opcode stubs

---
+++ cpu.cpp
@@ -0,0 +1,22 @@
+//
+// Created by root on 8/26/17.
+//
+
+#include <cstring>
+#include "cpu.h"
+
+Cpu::Cpu() {
+}
+
+void Cpu::loadGame(const char *filename) {
+
+}
+
+void* Cpu::executeOpcode(unsigned char opcode) {
+    // each opcode is one-byte
+    switch(opcode) {
+
+    }
+}
+
+Cpu::~Cpu() {}
